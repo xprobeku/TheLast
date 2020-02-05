6b1f19ec1c5f033fe14349c541ebcb41c9f55feb
xpro <xprobeku@gmail.com>
2020-02-04 22:29:56 -0600

    [ADD] Car service and repository

---
+++ src/main/java/edu/mum/service/CarService.java
@@ -0,0 +1,17 @@
+package edu.mum.service;
+
+
+import edu.mum.domain.Car;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface CarService {
+    List<Car> getAll();
+    List<Car> getAllCarByOwner(long userId);
+    Car getById(Long id);
+    void save(Car car);
+    void updateCar(Car car);
+    void deleteCarById(Long carId);
+//    List<Car> searchCars(String searchString);
+}
---
+++ src/main/java/edu/mum/service/impl/CarServiceImpl.java
@@ -0,0 +1,72 @@
+package edu.mum.service.impl;
+
+
+import edu.mum.domain.Car;
+import edu.mum.repository.CarRepository;
+import edu.mum.service.CarService;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CarServiceImpl implements CarService {
+
+    @Autowired
+    private CarRepository carRepository;
+
+
+    @Override
+    public List<Car> getAll() {
+        return carRepository.findAll();
+    }
+
+    @Override
+    public List<Car> getAllCarByOwner(long userId) {
+        List<Car> cars = carRepository.findByOwnerId(userId);
+        return cars;
+    }
+
+    @Override
+    public Car getById(Long id) {
+        return carRepository.getById(id);
+    }
+
+    @Override
+    public void updateCar(Car car) {
+        carRepository.save(car);
+    }
+
+    @Override
+    public void save(Car car) {
+            carRepository.save(car);
+    }
+
+
+    @Override
+    public void deleteCarById(Long carId) {
+        carRepository.delete(carId);
+    }
+
+//    @Override
+//    public List<Car> searchCars(String searchString) {
+//        return carRepository.findAllByCarBrandContainingOrModelContainingOrDescriptionContainingOrderByDescription(searchString);
+//    }
+
+
+}
