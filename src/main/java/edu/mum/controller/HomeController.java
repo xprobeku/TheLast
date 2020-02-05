ee4e7286f2c92cf16f79917756bd7df54dcaa193
Edy <erdenebyr@gmail.com>
2020-02-04 22:39:56 -0600

    car

--- src/main/java/edu/mum/controller/HomeController.java
+++ src/main/java/edu/mum/controller/HomeController.java
@@ -1,8 +1,14 @@
 package edu.mum.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
