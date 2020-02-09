fba358be02dbce3af565b8e8fd65e1b37f348809
Meron2427 <33803258+Meron2427@users.noreply.github.com>
2020-02-09 11:41:20 -0600

    Update CarController.java

--- src/main/java/edu/mum/controller/CarController.java
+++ src/main/java/edu/mum/controller/CarController.java
@@ -18,23 +18,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 @Controller
 public class CarController {
     @Autowired
     CarService carService;
-    static String saveDirectory = "uploadedFiles";
+
     @RequestMapping("/carform")
     public String showCars(@ModelAttribute("car")Car car, Model model){
         model.addAttribute("brandList", carService.getBrands());
@@ -52,24 +45,11 @@ public class CarController {
     }
 
     @RequestMapping(value="/save", method=RequestMethod.POST)
-    public String save(@Valid @ModelAttribute("car") Car car, BindingResult result, HttpSession session, HttpServletRequest request)  {
+    public String save(@Valid @ModelAttribute("car") Car car, BindingResult result, HttpSession session){
         if(result.hasErrors()){
             return "carform";
         }
-        
-    	MultipartFile productImage = car.getProductImage();
-		String rootDirectory = request.getSession().getServletContext().getRealPath("/");
-				
-			if (productImage!=null && !productImage.isEmpty()) {
-		       try {
-			      	productImage.transferTo(new File(rootDirectory + File.separator  + "resources" + File.separator  
-			                   + "images" + File.separator + car.getId() + ".png"));
-			      	System.out.println(productImage);
-		       } catch (Exception e) {
-				throw new RuntimeException("Product Image saving failed", e);
-		   }
-		   }
-        
+
         car.setOwner((User)session.getAttribute("user"));
         car.setAvailable(true);
         car.setStatus("PENDING");
