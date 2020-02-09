a9243b1fd83ad306a9a83d8322b11a7b29a86159
Meron2427 <33803258+Meron2427@users.noreply.github.com>
2020-02-09 09:26:49 -0600

    Update CarController.java

--- src/main/java/edu/mum/controller/CarController.java
+++ src/main/java/edu/mum/controller/CarController.java
@@ -18,16 +18,23 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
 public class CarController {
     @Autowired
     CarService carService;
-
+    static String saveDirectory = "uploadedFiles";
     @RequestMapping("/carform")
     public String showCars(@ModelAttribute("car")Car car, Model model){
         model.addAttribute("brandList", carService.getBrands());
@@ -40,16 +47,29 @@ public class CarController {
     public String approvecar(@ModelAttribute("car")Car car, Model model){
         model.addAttribute("brandList", carService.getBrands());
         car.setAvailable(true);
-        car.setStatus("ACTIVE");
+        car.setStatus("PENDING");
         return "carform";
     }
 
     @RequestMapping(value="/save", method=RequestMethod.POST)
-    public String save(@Valid @ModelAttribute("car") Car car, BindingResult result, HttpSession session){
+    public String save(@Valid @ModelAttribute("car") Car car, BindingResult result, HttpSession session, HttpServletRequest request)  {
         if(result.hasErrors()){
             return "carform";
         }
-
+        
+    	MultipartFile productImage = car.getProductImage();
+		String rootDirectory = request.getSession().getServletContext().getRealPath("/");
+				
+			if (productImage!=null && !productImage.isEmpty()) {
+		       try {
+			      	productImage.transferTo(new File(rootDirectory + File.separator  + "resources" + File.separator  
+			                   + "images" + File.separator + car.getId() + ".png"));
+			      	System.out.println(productImage);
+		       } catch (Exception e) {
+				throw new RuntimeException("Product Image saving failed", e);
+		   }
+		   }
+        
         car.setOwner((User)session.getAttribute("user"));
         car.setAvailable(true);
         car.setStatus("PENDING");
@@ -80,28 +100,16 @@ public class CarController {
         return "careditform";
     }
 
-    @RequestMapping(value="/editcar/editsave", method = RequestMethod.POST)
-    public String editSave(@Valid @ModelAttribute("car")Car car, BindingResult result, HttpSession session){
+    @RequestMapping(value="/editsave", method = RequestMethod.POST)
+    public String editSave(@Valid @ModelAttribute("car")Car car, BindingResult result){
         if(result.hasErrors())
-            return "careditform";
+            return "/editcar/{id}";
         //change after session complete * ADMIN can change
         car.setAvailable(true);
         car.setStatus("PENDING");
 
         carService.updateCar(car);
         return "redirect:/viewcar";
-    }
-
-    @RequestMapping(value="/approve/{id}", method = RequestMethod.GET)
-    public String editSave( @ModelAttribute("car")Car car, BindingResult result){
-        if(result.hasErrors())
-            return "careditform";
-        //change after session complete * ADMIN can change
-        car.setAvailable(true);
-        car.setStatus("ACTIVE");
-
-//        carService.updateCar(car);
-        return "viewapprovecar";
     }
 
     @RequestMapping(value="/deletecar/{id}", method = RequestMethod.GET)
